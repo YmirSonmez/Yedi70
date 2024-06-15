f48796f6e863065bf5d7c09f29c36c2366c3ffd0
YmirSG <70720009+YmirSonmez@users.noreply.github.com>
2024-06-15 16:41:24 +0300

    Update reklam.js

--- reklam.js
+++ reklam.js
@@ -1,1 +1,1 @@
-{"reklam":"https://github.com/YmirSonmez/Yedi70/blob/main/photo_2024-06-15_16-28-14.jpg"}
+{"reklam":"https://raw.github.com/YmirSonmez/Yedi70/blob/main/photo_2024-06-15_16-28-14.jpg"}
