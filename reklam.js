30eca1e326d60eae34a539582a90d91552aaa808
YmirSG <70720009+YmirSonmez@users.noreply.github.com>
2024-06-16 22:17:22 +0300

    Update reklam.js

--- reklam.js
+++ reklam.js
@@ -1,1 +1,9 @@
-{"reklam":"https://github.com/YmirSonmez/Yedi70/blob/main/r.jpg?raw=true","link":"https://t.me/BlumFarmBott/1645/1746","counter":"https://tinyurl.com/ts38m9tk"}
+{"reklam":"https://github.com/YmirSonmez/Yedi70/blob/main/r.jpg?raw=true","link":"https://t.me/BlumFarmBott/1645/1746","counter":"https://tinyurl.com/ts38m9tk",
+  "button_1_name":"Yedi70 Telegram",
+  "button_1_link":"https://t.me/+wC0eF2DdiZ81MTNk",
+  "button_2_name":"Yedi70 Instagram",
+  "button_2_link":"https://www.instagram.com/yedi70kazan/",
+  "button_3_name":"Bot Güncellemeleri",
+  "button_3_link":"https://t.me/blumfarmbott",
+  "button_4_name":"WiKi",
+  "button_4_link":"https://ymirsonmez.github.io/Yedi70.github.io/"}
