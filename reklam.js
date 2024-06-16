cb00c68cdd94651ce9227f0f0ffdbc0d41afd3b4
YmirSG <70720009+YmirSonmez@users.noreply.github.com>
2024-06-16 22:03:19 +0300

    Update reklam.js

--- reklam.js
+++ reklam.js
@@ -1,1 +1,1 @@
-{"reklam":"https://github.com/YmirSonmez/Yedi70/blob/main/r.jpg?raw=true","link":"https://t.me/c/2136336618/253","counter":"https://tinyurl.com/ts38m9tk"}
+{"reklam":"https://github.com/YmirSonmez/Yedi70/blob/main/r.jpg?raw=true","link":"https://t.me/BlumFarmBott/1645/1746","counter":"https://tinyurl.com/ts38m9tk"}
