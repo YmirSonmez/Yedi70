9710ab83759a7238e56219e6735f02640ff49b1d
YmirSG <70720009+YmirSonmez@users.noreply.github.com>
2024-06-19 19:48:27 +0300

    Update reklam.js

--- reklam.js
+++ reklam.js
@@ -9,4 +9,5 @@
   "button_4_link":"https://ymirsonmez.github.io/Yedi70.github.io/",
   "button_5_name":"TG Desktop",
   "button_5_link":"https://desktop.telegram.org/",
-  "bot_active":"1"}
+  "bot_active":"1",
+  "active":"1"}
